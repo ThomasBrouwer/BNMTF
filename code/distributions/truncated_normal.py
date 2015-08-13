75d6013efb16b51053ea558295c1ee7f7e0a6b4f
Thomas Brouwer <tab43@cam.ac.uk>
2015-08-13 16:19:29 +0100

    Replaced scipy truncnorm by other library for more efficient draws - will see what difference it makes.

--- code/distributions/truncated_normal.py
+++ code/distributions/truncated_normal.py
@@ -15,8 +15,9 @@ inf we actually want it to be zero. We then also get variance inf, but that
 would result in 0's for all Uik, Vjk, Fik, Skl, Gjl dependent on that entry -
 we actually want variance 0 (so we get 0 with 100% guarantee).
 """
-import math, numpy
+import math, numpy, time
 from scipy.stats import truncnorm, norm
+import rtnorm
 
 class TruncatedNormal:
     def __init__(self,mu,tau):
@@ -28,38 +29,59 @@ class TruncatedNormal:
         
     # Draw a value for x ~ TruncatedNormal(mu,tau). If we get inf we set it to 0.
     def draw(self):
-        d = truncnorm.rvs(a=self.a, b=self.b, loc=self.mu, scale=self.sigma, size=None)
-        return d if (d >= 0.0 and d != numpy.inf and d != -numpy.inf and not numpy.isnan(d)) else 0.
+        if self.tau == 0.:
+            return 0.
+        #d = truncnorm.rvs(a=self.a, b=self.b, loc=self.mu, scale=self.sigma, size=None)
+        d = rtnorm.rtnorm(a=0., b=numpy.inf, mu=self.mu, sigma=self.sigma)[0]
+        return d if (d >= 0. and d != numpy.inf and d != -numpy.inf and not numpy.isnan(d)) else 0.
         
     # Return expectation. x = - self.mu / self.sigma; lambdax = norm.pdf(x)/(1-norm.cdf(x)); return self.mu + self.sigma * lambdax
     def expectation(self):
+        if self.tau == 0.:
+            return 0.
         exp = truncnorm.stats(self.a, self.b, loc=self.mu, scale=self.sigma, moments='m')
         return exp if (exp >= 0.0 and exp != numpy.inf and exp != -numpy.inf and not numpy.isnan(exp)) else 0.
         
     # Return variance. The library gives NaN for this due to b->inf, so we compute it ourselves
     def variance(self):
+        if self.tau == 0.:
+            return 0.
         x = - self.mu / self.sigma
         lambdax = norm.pdf(x)/(1-norm.cdf(x))
         deltax = lambdax*(lambdax-x)
         var = self.sigma**2 * ( 1 - deltax )
         return var if (var >= 0.0 and var != numpy.inf and var != -numpy.inf and not numpy.isnan(var)) else 0.
         
-        
+    
 '''
 # Draw 10000 values and plot. Also plot pdf of Truncated Normal, and regular Normal.
 import matplotlib.pyplot as plt
 import scipy.stats as stats
 
+draws = 10000
 mu, sigma, tau = 2., 3., 1./9.
 lower, upper = (0-mu)/sigma, numpy.inf
-X = [TruncatedNormal(mu,tau).draw() for i in range(0,10000)]
-X2 = stats.truncnorm(
-    (lower - mu) / sigma, (upper - mu) / sigma, loc=mu, scale=sigma)
-N = stats.norm(loc=mu, scale=sigma)
 
-fig, ax = plt.subplots(3, sharex=True)
-ax[0].hist(X, normed=True)
-ax[1].hist(X2.rvs(10000), normed=True)
-ax[2].hist(N.rvs(10000), normed=True)
+time0 = time.time()
+# Our implementation
+X = [TruncatedNormal(mu,tau).draw() for i in range(0,draws)]
+time1 = time.time()
+# scipy truncnorm library
+X2 = stats.truncnorm(lower, upper, loc=mu, scale=sigma).rvs(draws)
+time2 = time.time()
+# C. Lassner efficient implementation
+X3 = rtnorm.rtnorm(a=0., b=numpy.inf, mu=mu, sigma=sigma, size=draws)
+time3 = time.time()
+# Draws from normal distribution
+N = stats.norm(loc=mu, scale=sigma).rvs(draws)
+time4 = time.time()
+
+print "Completed draws. Time taken: ours %s, scipy %s, lassner %s, normal %s." % (time1-time0,time2-time1,time3-time2,time4-time3)
+
+fig, ax = plt.subplots(4, sharex=True)
+ax[0].hist(X, bins=range(-20,20), normed=True)
+ax[1].hist(X2, bins=range(-20,20), normed=True)
+ax[2].hist(X3, bins=range(-20,20), normed=True)
+ax[3].hist(N, bins=range(-20,20), normed=True)
 plt.show()
 '''
