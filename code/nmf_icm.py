3e404abd9ae0696e904434a7f295320277147f3d
Thomas Brouwer <tab43@cam.ac.uk>
2015-09-30 11:54:28 +0100

    Added ICM NMF algorithm, and ran it on the toy and drug sensitivity dataset. Convergence is very fast but values tend to go to 0 and stay there - only good performance when you reset to 0.01.

---
+++ code/nmf_icm.py
@@ -0,0 +1,211 @@
+"""
+Iterated Conditional Modes for MAP non-negative matrix tri-factorisation.
+
+We expect the following arguments:
+- R, the matrix
+- M, the mask matrix indicating observed values (1) and unobserved ones (0)
+- K, the number of latent factors
+- priors = { 'alpha' = alpha_R, 'beta' = beta_R, 'lambdaU' = [[lambdaUik]], 'lambdaV' = [[lambdaVjk]] },
+    a dictionary defining the priors over tau, U, V.
+    
+Initialisation can be done by running the initialise() function, with argument init:
+- init='random' -> draw initial values randomly from priors Exp, Gamma
+- init='exp'    -> use the expectation of the priors Exp, Gamma
+Alternatively, you can define your own initial values for U, V, and tau.
+
+Usage of class:
+    NMF = nmf_icm(R,M,K,priors)
+    NMF.initisalise(init)
+    NMF.run(iterations)
+Or:
+    NMF = nmf_icm(R,M,K,priors)
+    NMF.train(init,iterations)
+
+We can test the performance of our model on a test dataset, specifying our test set with a mask M. 
+    performance = NMF.predict(M_pred)
+This gives a dictionary of performances,
+    performance = { 'MSE', 'R^2', 'Rp' }
+    
+The performances of all iterations are stored in NMF.all_performances, which 
+is a dictionary from 'MSE', 'R^2', or 'Rp' to a list of performances.
+    
+Finally, we can return the goodness of fit of the data using the quality(metric) function:
+- metric = 'loglikelihood' -> return p(D|theta)
+         = 'BIC'        -> return Bayesian Information Criterion
+         = 'AIC'        -> return Afaike Information Criterion
+         = 'MSE'        -> return Mean Square Error
+(we want to maximise these values)
+"""
+
+from distributions.exponential import exponential_draw
+from distributions.gamma import gamma_mode
+from distributions.truncated_normal_vector import TN_vector_mode
+
+import numpy, itertools, math, time
+
+minimum_TN = 0.01
+
+class nmf_icm:
+    def __init__(self,R,M,K,priors):
+        self.R = numpy.array(R,dtype=float)
+        self.M = numpy.array(M,dtype=float)
+        self.K = K
+        
+        assert len(self.R.shape) == 2, "Input matrix R is not a two-dimensional array, " \
+            "but instead %s-dimensional." % len(self.R.shape)
+        assert self.R.shape == self.M.shape, "Input matrix R is not of the same size as " \
+            "the indicator matrix M: %s and %s respectively." % (self.R.shape,self.M.shape)
+            
+        (self.I,self.J) = self.R.shape
+        self.size_Omega = self.M.sum()
+        self.check_empty_rows_columns()      
+        
+        self.alpha, self.beta, self.lambdaU, self.lambdaV = \
+            float(priors['alpha']), float(priors['beta']), numpy.array(priors['lambdaU']), numpy.array(priors['lambdaV'])
+        
+        assert self.lambdaU.shape == (self.I,self.K), "Prior matrix lambdaU has the wrong shape: %s instead of (%s, %s)." % (self.lambdaU.shape,self.I,self.K)
+        assert self.lambdaV.shape == (self.J,self.K), "Prior matrix lambdaV has the wrong shape: %s instead of (%s, %s)." % (self.lambdaV.shape,self.J,self.K)
+            
+        
+    # Raise an exception if an entire row or column is empty
+    def check_empty_rows_columns(self):
+        sums_columns = self.M.sum(axis=0)
+        sums_rows = self.M.sum(axis=1)
+                    
+        # Assert none of the rows or columns are entirely unknown values
+        for i,c in enumerate(sums_rows):
+            assert c != 0, "Fully unobserved row in R, row %s." % i
+        for j,c in enumerate(sums_columns):
+            assert c != 0, "Fully unobserved column in R, column %s." % j
+
+
+    # Initialise and run the sampler
+    def train(self,init,iterations):
+        self.initialise(init=init)
+        return self.run(iterations)
+
+
+    # Initialise U, V, and tau. If init='random', draw values from an Exp and Gamma distribution. If init='exp', set it to the expectation values.
+    def initialise(self,init='random'):
+        assert init in ['random','exp'], "Unknown initialisation option: %s. Should be 'random' or 'exp'." % init
+        self.U = numpy.zeros((self.I,self.K))
+        self.V = numpy.zeros((self.J,self.K))
+        
+        if init == 'random':
+            for i,k in itertools.product(xrange(0,self.I),xrange(0,self.K)):
+                self.U[i,k] = exponential_draw(self.lambdaU[i][k])
+            for j,k in itertools.product(xrange(0,self.J),xrange(0,self.K)):
+                self.V[j,k] = exponential_draw(self.lambdaV[j][k])
+            
+        elif init == 'exp':
+            for i,k in itertools.product(xrange(0,self.I),xrange(0,self.K)):
+                self.U[i,k] = 1.0/self.lambdaU[i][k]
+            for j,k in itertools.product(xrange(0,self.J),xrange(0,self.K)):
+                self.V[j,k] = 1.0/self.lambdaV[j][k]
+        
+        self.tau = self.alpha/self.beta
+       
+
+    # Run the Gibbs sampler
+    def run(self,iterations):   
+        self.all_tau = numpy.zeros(iterations) 
+        
+        metrics = ['MSE','R^2','Rp']
+        self.all_performances = {} # for plotting convergence of metrics
+        for metric in metrics:
+            self.all_performances[metric] = []
+        
+        for it in range(0,iterations):      
+            for k in range(0,self.K):   
+                tauUk = self.tauU(k)
+                muUk = self.muU(tauUk,k)
+                self.U[:,k] = TN_vector_mode(muUk) 
+                self.U[:,k] = numpy.maximum(self.U[:,k],minimum_TN*numpy.ones(self.I))
+                
+            self.tau = gamma_mode(self.alpha_s(),self.beta_s())
+            self.all_tau[it] = self.tau
+            
+            for k in range(0,self.K):
+                tauVk = self.tauV(k)
+                muVk = self.muV(tauVk,k)
+                self.V[:,k] = TN_vector_mode(muVk) 
+                self.V[:,k] = numpy.maximum(self.V[:,k],minimum_TN*numpy.ones(self.J))
+                
+            perf = self.predict(self.M)
+            for metric in metrics:
+                self.all_performances[metric].append(perf[metric])
+                
+            print "Iteration %s. MSE: %s. R^2: %s. Rp: %s." % (it+1,perf['MSE'],perf['R^2'],perf['Rp'])
+            
+        return
+        
+        
+    # Compute the parameters for the distributions we sample from
+    def alpha_s(self):   
+        return self.alpha + self.size_Omega/2.0
+    
+    def beta_s(self):   
+        return self.beta + 0.5*(self.M*(self.R-numpy.dot(self.U,self.V.T))**2).sum()
+        
+    def tauU(self,k):       
+        return self.tau*(self.M*self.V[:,k]**2).sum(axis=1)
+        
+    def muU(self,tauUk,k):
+        return 1./tauUk * (-self.lambdaU[:,k] + self.tau*(self.M * ( (self.R-numpy.dot(self.U,self.V.T)+numpy.outer(self.U[:,k],self.V[:,k]))*self.V[:,k] )).sum(axis=1)) 
+        
+    def tauV(self,k):
+        return self.tau*(self.M.T*self.U[:,k]**2).T.sum(axis=0)
+        
+    def muV(self,tauVk,k):
+        return 1./tauVk * (-self.lambdaV[:,k] + self.tau*(self.M.T * ( (self.R-numpy.dot(self.U,self.V.T)+numpy.outer(self.U[:,k],self.V[:,k])).T*self.U[:,k] )).T.sum(axis=0)) 
+
+
+    # Compute the expectation of U and V, and use it to predict missing values
+    def predict(self,M_pred):
+        R_pred = numpy.dot(self.U,self.V.T)
+        MSE = self.compute_MSE(M_pred,self.R,R_pred)
+        R2 = self.compute_R2(M_pred,self.R,R_pred)    
+        Rp = self.compute_Rp(M_pred,self.R,R_pred)        
+        return {'MSE':MSE,'R^2':R2,'Rp':Rp}
+        
+        
+    # Functions for computing MSE, R^2 (coefficient of determination), Rp (Pearson correlation)
+    def compute_MSE(self,M,R,R_pred):
+        return (M * (R-R_pred)**2).sum() / float(M.sum())
+        
+    def compute_R2(self,M,R,R_pred):
+        mean = (M*R).sum() / float(M.sum())
+        SS_total = float((M*(R-mean)**2).sum())
+        SS_res = float((M*(R-R_pred)**2).sum())
+        return 1. - SS_res / SS_total if SS_total != 0. else numpy.inf
+        
+    def compute_Rp(self,M,R,R_pred):
+        mean_real = (M*R).sum() / float(M.sum())
+        mean_pred = (M*R_pred).sum() / float(M.sum())
+        covariance = (M*(R-mean_real)*(R_pred-mean_pred)).sum()
+        variance_real = (M*(R-mean_real)**2).sum()
+        variance_pred = (M*(R_pred-mean_pred)**2).sum()
+        return covariance / float(math.sqrt(variance_real)*math.sqrt(variance_pred))
+        
+
+    # Functions for model selection, measuring the goodness of fit vs model complexity
+    def quality(self,metric):
+        assert metric in ['loglikelihood','BIC','AIC','MSE'], 'Unrecognised metric for model quality: %s.' % metric
+
+        log_likelihood = self.log_likelihood()        
+        if metric == 'loglikelihood':
+            return log_likelihood
+        elif metric == 'BIC':
+            # -2*loglikelihood + (no. free parameters * log(no data points))
+            return - 2 * log_likelihood + (self.I*self.K+self.J*self.K) * math.log(self.size_Omega)
+        elif metric == 'AIC':
+            # -2*loglikelihood + 2*no. free parameters
+            return - 2 * log_likelihood + 2 * (self.I*self.K+self.J*self.K)
+        elif metric == 'MSE':
+            R_pred = numpy.dot(self.U,self.V.T)
+            return self.compute_MSE(self.M,self.R,R_pred)
+        
+    def log_likelihood(self):
+        # Return the likelihood of the data given the trained model's parameters
+        return self.size_Omega / 2. * ( math.log(self.tau) - math.log(2*math.pi) ) \
+             - self.tau / 2. * (self.M*( self.R - numpy.dot(self.U,self.V.T))**2).sum()
---
+++ tests/code/test_nmf_icm.py
@@ -0,0 +1,284 @@
+"""
+Tests for the Iterated Conditional Modes algorithm.
+"""
+
+import numpy, math, pytest, itertools
+from BNMTF.code.nmf_icm import nmf_icm
+
+
+""" Test constructor """
+def test_init():
+    # Test getting an exception when R and M are different sizes, and when R is not a 2D array.
+    R1 = numpy.ones(3)
+    M = numpy.ones((2,3))
+    I,J,K = 5,3,1
+    lambdaU = numpy.ones((I,K))
+    lambdaV = numpy.ones((J,K))
+    alpha, beta = 3, 1    
+    priors = { 'alpha':alpha, 'beta':beta, 'lambdaU':lambdaU, 'lambdaV':lambdaV }
+    
+    with pytest.raises(AssertionError) as error:
+        nmf_icm(R1,M,K,priors)
+    assert str(error.value) == "Input matrix R is not a two-dimensional array, but instead 1-dimensional."
+    
+    R2 = numpy.ones((4,3,2))
+    with pytest.raises(AssertionError) as error:
+        nmf_icm(R2,M,K,priors)
+    assert str(error.value) == "Input matrix R is not a two-dimensional array, but instead 3-dimensional."
+    
+    R3 = numpy.ones((3,2))
+    with pytest.raises(AssertionError) as error:
+        nmf_icm(R3,M,K,priors)
+    assert str(error.value) == "Input matrix R is not of the same size as the indicator matrix M: (3, 2) and (2, 3) respectively."
+    
+    # Similarly for lambdaU, lambdaV
+    R4 = numpy.ones((2,3))
+    lambdaU = numpy.ones((2+1,1))
+    priors = { 'alpha':alpha, 'beta':beta, 'lambdaU':lambdaU, 'lambdaV':lambdaV }
+    with pytest.raises(AssertionError) as error:
+        nmf_icm(R4,M,K,priors)
+    assert str(error.value) == "Prior matrix lambdaU has the wrong shape: (3, 1) instead of (2, 1)."
+    
+    lambdaU = numpy.ones((2,1))
+    lambdaV = numpy.ones((3+1,1))
+    priors = { 'alpha':alpha, 'beta':beta, 'lambdaU':lambdaU, 'lambdaV':lambdaV }
+    with pytest.raises(AssertionError) as error:
+        nmf_icm(R4,M,K,priors)
+    assert str(error.value) == "Prior matrix lambdaV has the wrong shape: (4, 1) instead of (3, 1)."
+    
+    # Test getting an exception if a row or column is entirely unknown
+    lambdaU = numpy.ones((2,1))
+    lambdaV = numpy.ones((3,1))
+    M1 = [[1,1,1],[0,0,0]]
+    M2 = [[1,1,0],[1,0,0]]
+    priors = { 'alpha':alpha, 'beta':beta, 'lambdaU':lambdaU, 'lambdaV':lambdaV }
+    
+    with pytest.raises(AssertionError) as error:
+        nmf_icm(R4,M1,K,priors)
+    assert str(error.value) == "Fully unobserved row in R, row 1."
+    with pytest.raises(AssertionError) as error:
+        nmf_icm(R4,M2,K,priors)
+    assert str(error.value) == "Fully unobserved column in R, column 2."
+    
+    # Finally, a successful case
+    I,J,K = 3,2,2
+    R5 = 2*numpy.ones((I,J))
+    lambdaU = numpy.ones((I,K))
+    lambdaV = numpy.ones((J,K))
+    M = numpy.ones((I,J))
+    priors = { 'alpha':alpha, 'beta':beta, 'lambdaU':lambdaU, 'lambdaV':lambdaV }
+    NMF = nmf_icm(R5,M,K,priors)
+    
+    assert numpy.array_equal(NMF.R,R5)
+    assert numpy.array_equal(NMF.M,M)
+    assert NMF.I == I
+    assert NMF.J == J
+    assert NMF.K == K
+    assert NMF.size_Omega == I*J
+    assert NMF.alpha == alpha
+    assert NMF.beta == beta
+    assert numpy.array_equal(NMF.lambdaU,lambdaU)
+    assert numpy.array_equal(NMF.lambdaV,lambdaV)
+    
+    
+""" Test initialing parameters """
+def test_initialise():
+    I,J,K = 5,3,2
+    R = numpy.ones((I,J))
+    M = numpy.ones((I,J))
+    
+    lambdaU = 2*numpy.ones((I,K))
+    lambdaV = 3*numpy.ones((J,K))
+    alpha, beta = 3, 1
+    priors = { 'alpha':alpha, 'beta':beta, 'lambdaU':lambdaU, 'lambdaV':lambdaV }
+    
+    # First do a random initialisation - we can then only check whether values are correctly initialised
+    init = 'random'
+    NMF = nmf_icm(R,M,K,priors)
+    NMF.initialise(init)
+    
+    assert NMF.tau >= 0.0
+    for i,k in itertools.product(xrange(0,I),xrange(0,K)):
+        assert NMF.U[i,k] >= 0.0
+    for j,k in itertools.product(xrange(0,J),xrange(0,K)):
+        assert NMF.V[j,k] >= 0.0
+    assert NMF.tau == 3./1.
+        
+    # Then initialise with expectation values
+    init = 'exp'
+    NMF = nmf_icm(R,M,K,priors)
+    NMF.initialise(init)
+    
+    assert NMF.tau >= 0.0
+    for i,k in itertools.product(xrange(0,I),xrange(0,K)):
+        assert NMF.U[i,k] == 1./2.
+    for j,k in itertools.product(xrange(0,J),xrange(0,K)):
+        assert NMF.V[j,k] == 1./3.
+    assert NMF.tau == 3./1.
+    
+    
+""" Test computing values for alpha, beta, mu, tau. """
+I,J,K = 5,3,2
+R = numpy.ones((I,J))
+M = numpy.ones((I,J))
+M[0,0], M[2,2], M[3,1] = 0, 0, 0
+
+lambdaU = 2*numpy.ones((I,K))
+lambdaV = 3*numpy.ones((J,K))
+alpha, beta = 3, 1
+priors = { 'alpha':alpha, 'beta':beta, 'lambdaU':lambdaU, 'lambdaV':lambdaV }
+init = 'exp' #U=1/2,V=1/3
+
+def test_alpha_s():
+    NMF = nmf_icm(R,M,K,priors)
+    NMF.initialise(init)
+    alpha_s = alpha + 6.
+    assert NMF.alpha_s() == alpha_s
+
+def test_beta_s():
+    NMF = nmf_icm(R,M,K,priors)
+    NMF.initialise(init)
+    beta_s = beta + .5*(12*(2./3.)**2) #U*V.T = [[1/6+1/6,..]]
+    assert abs(NMF.beta_s() - beta_s) < 0.000000000000001
+    
+def test_tauU():
+    NMF = nmf_icm(R,M,K,priors)
+    NMF.initialise(init)
+    #V^2 = [[1/9,1/9],[1/9,1/9],[1/9,1/9]], sum_j V^2 = [2/9,1/3,2/9,2/9,1/3] (index=i)
+    tauU = 3.*numpy.array([[2./9.,2./9.],[1./3.,1./3.],[2./9.,2./9.],[2./9.,2./9.],[1./3.,1./3.]])
+    for i,k in itertools.product(xrange(0,I),xrange(0,K)):
+        assert NMF.tauU(k)[i] == tauU[i,k]
+        
+def test_muU():
+    NMF = nmf_icm(R,M,K,priors)
+    NMF.initialise(init)
+    #U*V^T - Uik*Vjk = [[1/6,..]], so Rij - Ui * Vj + Uik * Vjk = 5/6
+    tauU = 3.*numpy.array([[2./9.,2./9.],[1./3.,1./3.],[2./9.,2./9.],[2./9.,2./9.],[1./3.,1./3.]])
+    muU = 1./tauU * ( 3. * numpy.array([[2.*(5./6.)*(1./3.),10./18.],[15./18.,15./18.],[10./18.,10./18.],[10./18.,10./18.],[15./18.,15./18.]]) - lambdaU )
+    for i,k in itertools.product(xrange(0,I),xrange(0,K)):
+        assert abs(NMF.muU(tauU[:,k],k)[i] - muU[i,k]) < 0.000000000000001
+        
+def test_tauV():
+    NMF = nmf_icm(R,M,K,priors)
+    NMF.initialise(init)
+    #U^2 = [[1/4,1/4],[1/4,1/4],[1/4,1/4],[1/4,1/4],[1/4,1/4]], sum_i U^2 = [1,1,1] (index=j)
+    tauV = 3.*numpy.array([[1.,1.],[1.,1.],[1.,1.]])
+    for j,k in itertools.product(xrange(0,J),xrange(0,K)):
+        assert NMF.tauV(k)[j] == tauV[j,k]
+        
+def test_muV():
+    NMF = nmf_icm(R,M,K,priors)
+    NMF.initialise(init)
+    #U*V^T - Uik*Vjk = [[1/6,..]], so Rij - Ui * Vj + Uik * Vjk = 5/6
+    tauV = 3.*numpy.array([[1.,1.],[1.,1.],[1.,1.]])
+    muV = 1./tauV * ( 3. * numpy.array([[4.*(5./6.)*(1./2.),4.*(5./6.)*(1./2.)],[4.*(5./6.)*(1./2.),4.*(5./6.)*(1./2.)],[4.*(5./6.)*(1./2.),4.*(5./6.)*(1./2.)]]) - lambdaV )
+    for j,k in itertools.product(xrange(0,J),xrange(0,K)):
+        assert NMF.muV(tauV[:,k],k)[j] == muV[j,k]
+        
+        
+""" Test some iterations, and that the values have changed in U and V. """
+def test_run():
+    I,J,K = 10,5,2
+    R = numpy.ones((I,J))
+    M = numpy.ones((I,J))
+    M[0,0], M[2,2], M[3,1] = 0, 0, 0
+    
+    lambdaU = 2*numpy.ones((I,K))
+    lambdaV = 3*numpy.ones((J,K))
+    alpha, beta = 3, 1
+    priors = { 'alpha':alpha, 'beta':beta, 'lambdaU':lambdaU, 'lambdaV':lambdaV }
+    init = 'exp' #U=1/2,V=1/3
+    
+    iterations = 15
+    
+    NMF = nmf_icm(R,M,K,priors)
+    NMF.initialise(init)
+    NMF.run(iterations)
+    
+    assert NMF.all_tau.shape == (iterations,)
+    assert NMF.all_tau[1] != alpha/float(beta)
+
+    
+""" Test computing the performance of the predictions using the expectations """
+def test_predict():
+    (I,J,K) = (5,3,2)
+    U = numpy.array([[125.,126.],[126.,126.],[126.,126.],[126.,126.],[126.,126.]])
+    V = numpy.array([[84.,84.],[84.,84.],[84.,84.]])
+    taus = [m**2 for m in range(1,10+1)]
+    #R_pred = numpy.array([[21084.,21084.,21084.],[ 21168.,21168.,21168.],[21168.,21168.,21168.],[21168.,21168.,21168.],[21168.,21168.,21168.]])
+    
+    R = numpy.array([[1,2,3],[4,5,6],[7,8,9],[10,11,12],[13,14,15]],dtype=float)
+    M = numpy.ones((I,J))
+    lambdaU = 2*numpy.ones((I,K))
+    lambdaV = 3*numpy.ones((J,K))
+    alpha, beta = 3, 1
+    priors = { 'alpha':alpha, 'beta':beta, 'lambdaU':lambdaU, 'lambdaV':lambdaV }
+    
+    M_test = numpy.array([[0,0,1],[0,1,0],[0,0,0],[1,1,0],[0,0,0]]) #R->3,5,10,11, P_pred->21084,21168,21168,21168
+    MSE = (444408561. + 447872569. + 447660964. + 447618649) / 4.
+    R2 = 1. - (444408561. + 447872569. + 447660964. + 447618649) / (4.25**2+2.25**2+2.75**2+3.75**2) #mean=7.25
+    Rp = 357. / ( math.sqrt(44.75) * math.sqrt(5292.) ) #mean=7.25,var=44.75, mean_pred=21147,var_pred=5292, corr=(-4.25*-63 + -2.25*21 + 2.75*21 + 3.75*21)
+    
+    NMF = nmf_icm(R,M,K,priors)
+    NMF.U = U
+    NMF.V = V
+    NMF.all_tau = taus
+    performances = NMF.predict(M_test)
+    
+    assert performances['MSE'] == MSE
+    assert performances['R^2'] == R2
+    assert performances['Rp'] == Rp
+    
+    
+""" Test the evaluation measures MSE, R^2, Rp """
+def test_compute_statistics():
+    R = numpy.array([[1,2],[3,4]],dtype=float)
+    M = numpy.array([[1,1],[0,1]])
+    I, J, K = 2, 2, 3
+    lambdaU = 2*numpy.ones((I,K))
+    lambdaV = 3*numpy.ones((J,K))
+    alpha, beta = 3, 1
+    priors = { 'alpha':alpha, 'beta':beta, 'lambdaU':lambdaU, 'lambdaV':lambdaV }
+    
+    BNMF = nmf_icm(R,M,K,priors)
+    
+    R_pred = numpy.array([[500,550],[1220,1342]],dtype=float)
+    M_pred = numpy.array([[0,0],[1,1]])
+    
+    MSE_pred = (1217**2 + 1338**2) / 2.0
+    R2_pred = 1. - (1217**2+1338**2)/(0.5**2+0.5**2) #mean=3.5
+    Rp_pred = 61. / ( math.sqrt(.5) * math.sqrt(7442.) ) #mean=3.5,var=0.5,mean_pred=1281,var_pred=7442,cov=61
+    
+    assert MSE_pred == BNMF.compute_MSE(M_pred,R,R_pred)
+    assert R2_pred == BNMF.compute_R2(M_pred,R,R_pred)
+    assert Rp_pred == BNMF.compute_Rp(M_pred,R,R_pred)
+    
+    
+""" Test the model quality measures. """
+def test_log_likelihood():
+    R = numpy.array([[1,2],[3,4]],dtype=float)
+    M = numpy.array([[1,1],[0,1]])
+    I, J, K = 2, 2, 3
+    lambdaU = 2*numpy.ones((I,K))
+    lambdaV = 3*numpy.ones((J,K))
+    alpha, beta = 3, 1
+    priors = { 'alpha':alpha, 'beta':beta, 'lambdaU':lambdaU, 'lambdaV':lambdaV }
+    
+    BNMF = nmf_icm(R,M,K,priors)
+    BNMF.U = numpy.ones((I,K))
+    BNMF.V = 2*numpy.ones((J,K))
+    BNMF.tau = 3.
+    # expU*expV.T = [[6.]]
+    
+    log_likelihood = 3./2.*(math.log(3.)-math.log(2*math.pi)) - 3./2. * (5**2 + 4**2 + 2**2)
+    AIC = -2*log_likelihood + 2*(2*3+2*3)
+    BIC = -2*log_likelihood + (2*3+2*3)*math.log(3)
+    MSE = (5**2+4**2+2**2)/3.
+    
+    assert log_likelihood == BNMF.quality('loglikelihood')
+    assert AIC == BNMF.quality('AIC')
+    assert BIC == BNMF.quality('BIC')
+    assert MSE == BNMF.quality('MSE')
+    with pytest.raises(AssertionError) as error:
+        BNMF.quality('FAIL')
+    assert str(error.value) == "Unrecognised metric for model quality: FAIL."
